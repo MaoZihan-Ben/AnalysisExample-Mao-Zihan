5659ce3f3204409d881eb0a2156478eed27131fc
MaoZihan-Ben <3044451272@qq.com>
2025-10-09 18:14:59 +0800

    Update Lagged model.py

--- Lagged model.py
+++ Lagged model.py
@@ -1,8 +1,21 @@
 """Lagged model:Endogeneity Handling Logic, Result Stability Verification"""
+"""Since these codes were extracted from my work records, 
+I imported all the libraries in every section at once :)."""
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
 import statsmodels.api as sm
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score, r2_score, mean_squared_error, mean_absolute_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
+import warnings
+
 independent_path = r"C:\Users\30444\Desktop\data\cleaned data\cleaned_data1.csv"
 dependent_path = r"C:\Users\30444\Desktop\data\cleaned data\cleaned_data 2.csv"
 control_path = r"C:\Users\30444\Desktop\data\cleaned data\cleaned_contorl.csv"  
