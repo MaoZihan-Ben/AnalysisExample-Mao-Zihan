2482bb33e857c0b5ea81ce2c033ec6d02b1338b2
MaoZihan-Ben <3044451272@qq.com>
2025-10-09 18:20:23 +0800

    Update Random Forest.py

--- Random Forest.py
+++ Random Forest.py
@@ -1,8 +1,19 @@
 """Random Forest(Non-linear relationship &Characteristic coefficient measurement)"""
+"""Since these codes were extracted from my work records, 
+I imported all the libraries in every section at once :)."""
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import statsmodels.api as sm
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score, r2_score, mean_squared_error, mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
 import warnings
 warnings.filterwarnings('ignore')
 
